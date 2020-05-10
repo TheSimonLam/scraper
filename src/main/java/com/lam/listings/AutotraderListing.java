b84d683f350780aed63ace85500324056c3616c0
Simon Lam <thesimonlam@gmail.com>
2020-05-10 22:15:41 +0100

    Making things modular

---
+++ src/main/java/com/lam/listings/AutotraderListing.java
@@ -0,0 +1,27 @@
+package com.lam.listings;
+
+import java.io.Serializable;
+
+public class AutotraderListing implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    private String title;
+    
+    public AutotraderListing() {}
+    public AutotraderListing(String title) {
+        super();
+        this.title = title;
+    }
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
